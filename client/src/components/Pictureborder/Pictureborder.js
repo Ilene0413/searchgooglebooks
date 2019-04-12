999e806370ee85bcb1ee6df767a3808c90bfe524
Ilene Cohen <ilene413@icloud.com>
2019-04-12 12:56:39 -0400

    sizing pictures

--- client/src/components/Pictureborder/Pictureborder.js
+++ client/src/components/Pictureborder/Pictureborder.js
@@ -4,10 +4,8 @@ function Pictureborder() {
     return (
             <div class="mx-auto ">
                 <img src="images/books1.jpg" alt="books width=90 height=200" ></img>
-                <img src="images/books2.jpg" alt="books width=90 height=200" ></img>
                 <img src="images/books3.jpg" alt="books width=90 height=200" ></img>
                 <img src="images/books3.jpg" alt="bookswidth=90 height=200" ></img>
-                <img src="images/books2.jpg" alt="books width=90 height=200" ></img>
                 <img src="images/books1.jpg" alt="books width=90 height=200" ></img>
             </div>
     );
