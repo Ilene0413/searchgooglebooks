fd270d53ba0a9742711913a8f13766ed00881fc4
Ilene Cohen <ilene413@icloud.com>
2019-04-11 22:17:47 -0400

    build

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-let PATH = process.env.NODE_ENV === "production" ? "../client/build/public/index.html" : "../client/public/index.html"
+	const PATH = process.env.NODE_ENV === "production" ? "../client/build/index.html" : "../client/public/index.html"
 	res.sendFile(path.join(__dirname, PATH));
 });
 
